793e282f6e251f8f17092c43b7adb3bfa2d7a75a
MounikaBarri <mounikabarri0@gmail.com>
2025-07-27 23:09:40 +0530

    chaged server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/api/auth', authRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
